f5334eb44338d96d95f388bc2626fbf49aa7dffe
wilnn <thangnguyen15700@gmail.com>
2025-10-15 16:02:26 -0400

    general update and clean up

--- my_model/modeling_mymodel.py
+++ my_model/modeling_mymodel.py
@@ -16,7 +16,7 @@ class GradReverse(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        return (-ctx.grl_lambda)*grad_output.neg(), None
+        return (-ctx.grl_lambda)*grad_output, None
     
 
 class GRL(nn.Module):
@@ -37,25 +37,26 @@ class MyModelVisionEncoder(nn.Module):
             config_ = AutoConfig.from_pretrained(config.vision_encoder_path)
             self.vision_encoder = AutoModel.from_config(config_).vision_model
     
-    def forward(self, pixel_values):
-        return self.vision_encoder(pixel_values)
+    def forward(self, pixel_values, **kwargs):
+        return self.vision_encoder(pixel_values, **kwargs)
 
-# TODO: CHANGE TO BERT IF SLIPLP TEXT MODEL TOKE LIMIT IS TOO SMALL (ONLY 77)
 class MyModelTextEncoder(nn.Module):
     def __init__(self, config, text_encoder=None):
         super().__init__()
         if text_encoder:
+            #print(type(text_encoder))
+            #exit(0)
             self.text_encoder = text_encoder
         else:
             config_ = AutoConfig.from_pretrained(config.text_encoder_path)
             #self.text_encoder = AutoModel.from_config(config_).text_model # FOR SIGLIP TEXT ENCODER
             
-            self.text_encoder = RobertaModel.from_config(config_) # FOR BERT
-                                                            # can not use AutoModel here because the `architectures`
-                                                            # in the config specify BertModelForCTC instead of BertModel class
-                                                            # so if use AutoModel it will load BertModelForCTC model class 
-    def forward(self, input_ids):
-        return self.text_encoder(input_ids)
+            self.text_encoder = AutoModel.from_config(config_)
+            
+    def forward(self, input_ids, attention_mask, **kwargs):
+        return self.text_encoder(input_ids=input_ids,
+                                 attention_mask=attention_mask,
+                                 **kwargs,)
     
 class MyModelAudioEncoder(nn.Module):
     def __init__(self, config, audio_encoder=None):
@@ -64,25 +65,36 @@ class MyModelAudioEncoder(nn.Module):
             self.audio_encoder = audio_encoder
         else:
             config_ = AutoConfig.from_pretrained(config.audio_encoder_path)
-            self.audio_encoder = Wav2Vec2Model.from_config(config_)
+            self.audio_encoder = AutoModel.from_config(config_)
     
-    def forward(self, audio, return_dict=False):
-        return self.audio_encoder(audio, return_dict=return_dict)
+    def forward(self, audio, return_dict=False, **kwargs):
+        return self.audio_encoder(audio,
+                                  return_dict=return_dict, **kwargs)
 
 
 class MyModelFFN(nn.Module):
-    def __init__(self, config, dims_per_layer: list[list]):
+    def __init__(self, config, dims_per_layer: list[list], last_act=True):
         super().__init__()
 
-        act_list = {"relu":nn.ReLU(),
-                    "gelu":nn.GELU(),
-                    "silu":nn.SiLU(),
-                    } # JUST USE GELU TO MATCH WITH THE PRETRAINED MODEL
+        _map = {"relu": nn.ReLU,
+                "gelu": nn.GELU,
+                "silu": nn.SiLU,
+                "dropout": nn.Dropout,
+                } # JUST USE GELU TO MATCH WITH THE PRETRAINED MODEL
 
-        self.FFN = nn.Sequential(*[layer
-                            for in_features, out_features in dims_per_layer
-                            for layer in (nn.Linear(in_features, out_features), act_list[config.hidden_act])
-                            ])
+        ls = []
+
+        for ll in dims_per_layer:
+            if type(ll) is not str:
+                #print(type(ll[0]), type(ll[1]))
+                #exit(0)
+                ls.append(nn.Linear(ll[0], ll[1]))
+            else:
+                ls.append(_map[config.hidden_act]())
+
+
+
+        self.FFN = nn.Sequential(*ls)
     def forward(self, x):
         return self.FFN(x)
 
@@ -150,40 +162,6 @@ class MyModelCrossAttention2(nn.Module):
         return self.out_proj(attn_out)
 '''
 
-class MyModelSelfAttention(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.embed_dim = config.hidden_size
-        self.num_attention_heads = config.num_attention_heads
-        self.attention_dropout = getattr(config, "attention_dropout", 0.0)
-
-        # batch_first=True makes input shape (B, L, E)
-        self.mha = nn.MultiheadAttention(
-            embed_dim=self.embed_dim,
-            num_heads=self.num_attention_heads,
-            dropout=self.attention_dropout,
-            batch_first=True
-        )
-
-    def forward(self, input_ids_embed,
-                key_attention_mask=None,  # shape (B, L_k), True=pad
-                need_weights=False):
-
-        # nn.MultiheadAttention expects float masks, True=pad
-        # It automatically handles Q/K/V projection
-        attn_output, attn_weights = self.mha(
-            query=input_ids_embed,      # (B, L_q, E)
-            key=input_ids_embed,     # (B, L_k, E)
-            value=input_ids_embed,   # (B, L_k, E)
-            key_padding_mask=key_attention_mask,  # True=pad positions
-            need_weights=need_weights
-        )
-
-        if need_weights:
-            return attn_output, attn_weights
-        else:
-            return attn_output
-
 class MyModelCrossAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -203,7 +181,7 @@ class MyModelCrossAttention(nn.Module):
                 key_attention_mask=None,  # shape (B, L_k), True=pad
                 need_weights=False):
 
-        # nn.MultiheadAttention expects float masks, True=pad
+        # nn.MultiheadAttention expects bool or float masks, True=pad
         # It automatically handles Q/K/V projection
         attn_output, attn_weights = self.mha(
             query=input_ids_embed,      # (B, L_q, E)
@@ -219,9 +197,11 @@ class MyModelCrossAttention(nn.Module):
             return attn_output
 
 @dataclass
-class MyModelSequenceClassifierOutput(SequenceClassifierOutput):
+class MyModelClassifierOutput(SequenceClassifierOutput):
     # use Optional[...] so it's backward-compatible when field is not present
     dc_logits: Optional[torch.Tensor] = None
+    dc_loss: Optional[torch.Tensor] = None
+    tc_loss: Optional[torch.Tensor] = None
 
 class MyModel(nn.Module):
     def __init__(self, config, vision_encoder=None, text_encoder=None,
@@ -264,23 +244,28 @@ class MyModel(nn.Module):
         audio_pooled_embed = self.audio_FFN(audio_pooled_embed)
 
         out = self.cross_attn(pixel_values_out.last_hidden_state,
-                              input_ids_out.pooler_output, # only do it with the pooler ouput since 
+                              input_ids_out.pooler_output.unsqueeze(1), # only do it with the pooler ouput since 
                                                         # we will only use it afterward
                               pixel_values_attention_mask)
         
-        # residual connection and layer norm
-        out = self.cross_attn_layer_norm(out + input_ids_out.last_hidden_state)
 
+        
+        # residual connection and layer norm
+        out = self.cross_attn_layer_norm(out + input_ids_out.pooler_output.unsqueeze(1))
+        #print(out.shape)
         #out = self.self_attn(out, input_ids_attention_mask)
         #out = self.self_attn_layer_norm(out + input_ids_out.last_hidden_state)
 
         out2 = self.cross_attn_FFN(out)
         # residual connection and layer norm
         out2 = self.cross_attn_FFN_layer_norm(out+out2)
+        #print(out2.shape)
+        #exit(0)
 
         # concat the pooled text embedding with the pooled audio embedding
-        out2 = torch.cat([out2, audio_pooled_embed], dim=-1)
-
+        out2 = torch.cat([out2.squeeze(1), audio_pooled_embed], dim=-1)
+        #print(out2.shape)
+        #exit(0)
         return self.task_classifier(out2)
 
     def forward(self, input_ids, pixel_values, audio,
@@ -290,6 +275,7 @@ class MyModel(nn.Module):
                 tc_labels =None,
                 return_dict=True,
                   **kwargs):
+
         
         # TODO: DONE
             # WORD2VEC2 AND WHISPER DO NOT HAVE POOLED OUTPUT.
@@ -302,38 +288,33 @@ class MyModel(nn.Module):
             # must do return_dict = True so that the word2vec model will 
             # return dict instead of tuple (only for this model. siglip do not
             # have this problem)
+        # TODO: DONE
+            # residual connection and layer norm(RMSnorm?) after attention
 
-        audio_out = self.audio_encoder(audio, return_dict=True).last_hidden_states
+
+        audio_out = self.audio_encoder(audio, return_dict=True)
         audio_hidden_states = audio_out.last_hidden_state
 
         audio_pooled_embed = audio_hidden_states.mean(dim=1)
-
+        #print(audio_pooled_embed.shape)
+        #exit(0)
         # feature extracted by convolutional in the model. Contain raw acounstic features
         #audio_feature = audio_out.extract_features
 
-        input_ids_out = self.text_encoder(input_ids)
+        input_ids_out = self.text_encoder(input_ids, input_ids_attention_mask)
 
         pixel_values_out = self.vision_encoder(pixel_values)
-        pixel_values_attention_mask = torch.ones((pixel_values_out.last_hidden_state.shape[0],
+        #print('#############')
+        #print(input_ids_out.pooler_output.unsqueeze(1).shape)
+        #exit(0)
+        
+        # zeros means non padding token in pytorch MHA
+        pixel_values_attention_mask = torch.zeros((pixel_values_out.last_hidden_state.shape[0],
                                                   pixel_values_out.last_hidden_state.shape[1]),
-                                                  dtype=torch.bool, device=pixel_values_out.device)
-        
-        if not self.training: # if not training
-            dc_logits = self.domain_classify(audio_pooled_embed)
-
-        tc_logits = self.task_classify(input_ids_out,
-                                       pixel_values_out,
-                                       audio_pooled_embed,
-                                       pixel_values_attention_mask,
-                                       input_ids_attention_mask)
-
-        
-
-        # TODO: DONE
-            # residual connection and layer norm(RMSnorm?) after attention
+                                                  dtype=torch.bool, device=pixel_values_out.last_hidden_state.device)
         
         # TODO: DONE
-        # only the text pooled emebddings will do the attention?  YES
+        # only the text pooled emebddings will do the attention
 
         # TODO: DONE
             # use image attention mask(like when image have padded patches)
@@ -349,31 +330,40 @@ class MyModel(nn.Module):
             # IF USE THE torch.nn.MultiheadAttention then need to flip the 
             # attention mask output by the model processor because hugginface 
             # 1 and 0 in the attention mask means the opposite
+            
+        dc_logits = None
+        if self.training or dc_labels is not None: # if training or given dc_labels(for evaluation)
+            dc_logits = self.domain_classify(audio_pooled_embed)
 
-
+        tc_logits = self.task_classify(input_ids_out,
+                                       pixel_values_out,
+                                       audio_pooled_embed,
+                                       pixel_values_attention_mask,
+                                       input_ids_attention_mask)
 
         dc_loss = 0
-        total_loss = 0
+        total_loss = None
         tc_loss = 0
 
-        if dc_labels:
-            #loss_fct = CrossEntropyLoss()
+        if dc_labels is not None:
             dc_loss = F.cross_entropy(dc_logits, dc_labels) # may have more than 2 domains/groups
 
-        if tc_labels:
-            tc_loss = F.binary_cross_entropy(F.sigmoid(tc_logits), tc_labels)
+        if tc_labels is not None:
+            tc_loss = F.binary_cross_entropy_with_logits(tc_logits, tc_labels)
 
-        if dc_labels or tc_labels:
+        if dc_labels  is not None or tc_labels is not None:
             total_loss = tc_loss + self.config.loss_lambda*dc_loss
 
         if not return_dict:
             output = (tc_logits, dc_logits)
             return ((total_loss,) + output) if total_loss is not None else output
 
-        return MyModelSequenceClassifierOutput(
+        return MyModelClassifierOutput(
             loss=total_loss,
             logits=tc_logits,
-            dc_logits=dc_logits,
             #hidden_states=None,
             #attentions=None,
+            dc_logits=dc_logits,
+            dc_loss=dc_loss,
+            tc_loss=tc_loss,
         )
--- my_model/processing_mymodel.py
+++ my_model/processing_mymodel.py
@@ -1,11 +1,5 @@
 # NEED TO SAMPLE THE AUDIO FILR TO 16000Hz before passing it to the audio model
 # processor and the audio model. because they original was train on 16000Hz
-# TODO: THE TEXT ENCODER IN SIGLIP AND CLIP ONLY HAVE 77 TOKEN LIMITS. USE BERT?
-
-# TODO: IMPORTANT
-# IF USE THE torch.nn.MultiheadAttention then need to flip the 
-# attention mask output by the model processor because hugginface 
-# 1 and 0 in the attention mask means the opposite
 
 from transformers import ProcessorMixin
 import re
@@ -48,33 +42,52 @@ class MyModelProcessor(ProcessorMixin):
         Args:
             audio (list[str]):
                 a list of string that are the paths to the audio files
-            image (list[str]):
+            images (list[str]):
                 a list of string that are the paths to the image files
             text (list[str]):
-                a list of string that are the paths to the text files
+                a list of string that are the text
+            tc_labels (list[int]):
+            dc_labels (list[int]):
 
         Returns:
-            
-
+            {
+                "audio":waveforms,
+                "pixel_values":pixel_values,
+                "input_ids":input_ids.input_ids,
+                "input_ids_attention_mask":input_ids.attention_mask,
+                "dc_labels": dc_labels,
+                "tc_labels":tc_labels,
+            }
         """
         waveforms = []
+        target_sample_rate = 16000 # whisper and wav2vec2 require the audio file being 16000 hz
         for audio_file in audio:
             waveform, sample_rate = torchaudio.load(audio_file)  # waveform shape: (channels(1), num_samples)
+            
+            # If stereo, convert to mono
+            if waveform.shape[0] > 1:
+                print("convert to mono")
+                waveform = torch.mean(waveform, dim=0, keepdim=True)
 
-            target_sample_rate = 16000 # whisper and wav2vec2 require the audio file being 16000 hz
             if sample_rate != target_sample_rate:
                 resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=target_sample_rate)
-                waveforms.append(resampler(waveform))
+                waveform = resampler(waveform)
+            
+            waveforms.append(waveform)
         
-        waveforms = self.feature_extractor.preprocess(waveforms,
-                                                      sampling_rate=target_sample_rate, 
-                                                      return_tensors="pt").input_values # return shape: [1, batch size, num channels(1), num_samples]
-        waveforms = waveforms.squeeze() # return tensor shape: (batch size, num_samples)
+        waveforms = self.feature_extractor(waveforms,
+                                            sampling_rate=target_sample_rate, 
+                                            return_tensors="pt").input_values # return shape: [1, batch size, num channels(1), num_samples]
+        
+        waveforms = torch.squeeze(waveforms, (0, 2)) # return tensor shape: (batch size, num_samples)
         
         pixel_values = self.image_processor.preprocess(images=[Image.open(path) for path in images], return_tensors="pt").pixel_values
-        #pixel_values_attention_mask = 
+
         input_ids = self.tokenizer(text, truncation=True, padding="longest", return_tensors='pt')
-                
+        
+        dc_labels = None if not dc_labels else torch.tensor(dc_labels, dtype=torch.long)
+        tc_labels = None if not tc_labels else torch.tensor(tc_labels, dtype=torch.float32).unsqueeze(-1)
+
         return {
                 "audio":waveforms,
                 "pixel_values":pixel_values,
@@ -84,7 +97,3 @@ class MyModelProcessor(ProcessorMixin):
                 "tc_labels":tc_labels,
                 }
 
-d = MyModelProcessor()
-d()
-a= 'asd'
-a.endswith()
