61eaa181ecd92a5b1740496d25ee5378044862d5
wilnn <thangnguyen15700@gmail.com>
2025-11-19 20:14:49 -0500

    add more custom train args

--- train_args.py
+++ train_args.py
@@ -4,3 +4,16 @@ from dataclasses import dataclass
 @dataclass
 class trainArgs(TrainingArguments):
     num_fold: int = 10
+    train_text_encoder: bool = False # the encoder part of the text encoder
+                                #(the pooler layer is always trained)
+    train_audio_encoder: bool = False
+    dataset_path: str = "./dataset/combined_dataset.csv"
+    audio_parent_path: str ="./dataset/taukadial/train/",
+    image_parent_path: str ="./dataset/images/images/",
+    pad_token: int = 0
+    dc_gender: bool = False
+    dc_language: bool = False
+    decision_threshold: float = 0.5
+    wandb_project_name: str = 'InvaCogni'
+    max_dataset_size: int = -1 # -1 means use full size.
+                            # > 0 means dataset of that many samples
